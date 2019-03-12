bc14bfc6fc11fb80de7c361f4a1ce5682fa0e164
ashwin74 <ashwin7411@gmail.com>
2019-03-12 23:04:28 +0530

    5 percent

--- app/src/main/java/com/example/ashwin/arshop/MainActivity.java
+++ app/src/main/java/com/example/ashwin/arshop/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.ashwin.arshop;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,86 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button about = (Button)findViewById(R.id.about);
+
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AboutUs.class);
+                startActivity(intent);
+            }
+        });
+
+        Button favourite = (Button)findViewById(R.id.favourite);
+
+        favourite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Favourite.class);
+                startActivity(intent);
+            }
+        });
+
+        Button n = (Button)findViewById(R.id.n);
+
+        n.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Notification.class);
+                startActivity(intent);
+            }
+        });
+
+        Button product = (Button)findViewById(R.id.product);
+
+        product.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ProductDetail.class);
+                startActivity(intent);
+            }
+        });
+
+        Button products = (Button)findViewById(R.id.products);
+
+        products.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ProductDisplay.class);
+                startActivity(intent);
+            }
+        });
+
+        Button signin = (Button)findViewById(R.id.signin);
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignIn.class);
+                startActivity(intent);
+            }
+        });
+
+        Button signup = (Button)findViewById(R.id.signup);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignUp.class);
+                startActivity(intent);
+            }
+        });
+
+
+        Button user = (Button)findViewById(R.id.user);
+
+        user.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, UserAccount.class);
+                startActivity(intent);
+            }
+        });
     }
 }
