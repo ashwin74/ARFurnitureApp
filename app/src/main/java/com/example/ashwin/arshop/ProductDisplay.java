24af0e5916cdd6c05ac0fe36e65b97c16bc854e4
ashwin74 <ashwin7411@gmail.com>
2019-03-16 12:16:45 +0530

    sort and search v1

--- app/src/main/java/com/example/ashwin/arshop/ProductDisplay.java
+++ app/src/main/java/com/example/ashwin/arshop/ProductDisplay.java
@@ -2,6 +2,8 @@ package com.example.ashwin.arshop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class ProductDisplay extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class ProductDisplay extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_display);
+
+        Spinner dropdown = findViewById(R.id.spinner1);
+        String[] items = new String[]{"Top Rated", "Price Low to High", "Price High to Low"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
     }
 }
